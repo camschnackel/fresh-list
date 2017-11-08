c3a7923c2d6624d67c8811afc5c08b351d73f1b6
Cam <cam.schnackel@gmail.com>
2017-11-08 08:47:46 -0600

    fully implemented loading circle for favorite/unfavoriting items. Began work on further mobile optimization

--- server/public/scripts/controllers/discover.controller.js
+++ server/public/scripts/controllers/discover.controller.js
@@ -1,55 +1,62 @@
 myApp.controller('DiscoverController', function (UserService, RecipeService, $scope) {
-    console.log('DiscoverController created');
-    var vm = this;
-    vm.userService = UserService;
-    vm.userObject = UserService.userObject;
-    vm.recipeObject = RecipeService.recipeObject;
-    vm.toggleState = {};
-    vm.hideCard = {};
-    vm.query = '';
-    RecipeService.recipeObject.suggested = '';
+  console.log('DiscoverController created');
+  var vm = this;
+  vm.userService = UserService;
+  vm.userObject = UserService.userObject;
+  vm.recipeObject = RecipeService.recipeObject;
+  vm.toggleState = {};
+  vm.hideCard = {};
+  vm.query = '';
+  vm.loading = false;
+  RecipeService.recipeObject.suggested = '';
 
-    vm.toggleIngredients = function (index) {
-        vm.toggleState[index] = {};
-        vm.toggleState[index].Ingredients = true;
-        vm.toggleState[index].Health = false;
-        vm.toggleState[index].Diet = false;
-    }
-    vm.toggleHealth = function (index) {
-        vm.toggleState[index] = {};
-        vm.toggleState[index].Ingredients = false;
-        vm.toggleState[index].Health = true;
-        vm.toggleState[index].Diet = false;
-    }
-    vm.toggleDiet = function (index) {
-        vm.toggleState[index] = {};
-        vm.toggleState[index].Ingredients = false;
-        vm.toggleState[index].Health = false;
-        vm.toggleState[index].Diet = true;
-    }
+  vm.stopLoad = function () {
+    vm.loading = false;
+  }
 
-    vm.getSaved = function () {
-        RecipeService.getSaved();
-        console.log('vm.recipeObject ->', vm.recipeObject);
-    }
+  vm.toggleIngredients = function (index) {
+    vm.toggleState[index] = {};
+    vm.toggleState[index].Ingredients = true;
+    vm.toggleState[index].Health = false;
+    vm.toggleState[index].Diet = false;
+  }
+  vm.toggleHealth = function (index) {
+    vm.toggleState[index] = {};
+    vm.toggleState[index].Ingredients = false;
+    vm.toggleState[index].Health = true;
+    vm.toggleState[index].Diet = false;
+  }
+  vm.toggleDiet = function (index) {
+    vm.toggleState[index] = {};
+    vm.toggleState[index].Ingredients = false;
+    vm.toggleState[index].Health = false;
+    vm.toggleState[index].Diet = true;
+  }
 
-    vm.favorite = function (recipe) {
-        console.log('favorite clicked w/ ->', recipe);
-        RecipeService.postRecipe(recipe);
-    };
+  vm.getSaved = function () {
+    RecipeService.getSaved();
+    console.log('vm.recipeObject ->', vm.recipeObject);
+  }
 
-    vm.unFavorite = function (id) {
-        console.log('unFavorite clicked w/ ->', id);
-        RecipeService.deleteRecipe(id);
-    };
+  vm.favorite = function (recipe) {
+    vm.loading = true;
+    console.log('favorite clicked w/ ->', recipe);
+    RecipeService.postRecipe(recipe).then(vm.stopLoad());
+  };
 
-    vm.search = function () {
-        RecipeService.search(vm.query).then(function () {
-            vm.query = '';
-        })
-    }
+  vm.unFavorite = function (id) {
+    vm.loading = true;
+    console.log('unFavorite clicked w/ ->', id);
+    RecipeService.deleteRecipe(id).then(vm.stopLoad());
+  };
 
-    vm.getSaved();
+  vm.search = function () {
+    RecipeService.search(vm.query).then(function () {
+      vm.query = '';
+    })
+  }
 
-    $scope.currentNavItem = 'discover';
+  vm.getSaved();
+
+  $scope.currentNavItem = 'discover';
 });
